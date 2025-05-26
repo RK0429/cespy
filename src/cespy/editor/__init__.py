cf956dc0eda5cae04038ba8d7e9ea5a277eaac4e
Ryota Kobayashi <s.woods.m.29@gmail.com>
2025-05-26 19:46:37 +0900

    UPD - Define public API in `__init__.py`
    This commit updates the `__init__.py` file by adding the `__all__` variable, which explicitly defines the public API of the module. This change enhances code clarity and helps users understand which classes are intended for public use, improving overall maintainability.

--- src/cespy/editor/__init__.py
+++ src/cespy/editor/__init__.py
@@ -1,3 +1,5 @@
 from .asc_editor import AscEditor
 from .qsch_editor import QschEditor
 from .spice_editor import SpiceCircuit, SpiceEditor
+
+__all__ = ["AscEditor", "QschEditor", "SpiceCircuit", "SpiceEditor"]
