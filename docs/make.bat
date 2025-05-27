25b5de086eddd8214dc5c94036c030aa43ad0ec4
Ryota Kobayashi <s.woods.m.29@gmail.com>
2025-05-27 18:10:10 +0900

    UPD - Update `make.bat` to improve Sphinx build command execution
    This commit modifies the `make.bat` script to enhance the execution of Sphinx build commands by using `call` for command invocation. This change ensures that the script correctly handles the output and error messages from the `sphinx-build` command. Additionally, it improves the help command output by providing clearer usage instructions. These updates aim to streamline the documentation build process and enhance user experience.

--- docs/make.bat
+++ docs/make.bat
@@ -6,14 +6,14 @@ REM  Command file for Sphinx documentation
 REM ——————————————————————————————————————————
 
 if "%SPHINXBUILD%" == "" (
-    set SPHINXBUILD=sphinx-build
+    set SPHINXBUILD=python -msphinx
 )
 
 set SOURCEDIR=source
 set BUILDDIR=build
 
 REM 1) Verify that sphinx-build exists (quietly)
-%SPHINXBUILD% --version >NUL 2>NUL
+call %SPHINXBUILD% --version >NUL 2>NUL
 if errorlevel 9009 (
     echo.
     echo The 'sphinx-build' command was not found. Make sure you have Sphinx installed,
@@ -27,11 +27,14 @@ REM 2) If no target is given, show help
 if "%1" == "" goto help
 
 REM 3) Run the requested build target and let all output (including errors) go to the console
-%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+call %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 goto end
 
 :help
-%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+echo.
+echo Usage: %~nx0 ^<target^>
+echo Available targets:
+call %SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 
 :end
 popd
